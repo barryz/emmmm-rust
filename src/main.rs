e5464be83201954ad30f164f16e50cc487795aa9
barryz <barryzxb@gmail.com>
2018-08-14 11:18:01 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    email: String,
+    age: u32,
+    address: String,
+}
+
+impl Person {
+    fn add_age(&mut self) {
+        self.age += 30;
+    }
+
+    fn older(&self, other: &Person) -> bool {
+        self.age > other.age
+    }
+}
+
 fn main() {
     let s_number = rand::thread_rng().gen_range(1, 101);
 
@@ -12,6 +31,36 @@ fn main() {
     println!("The result value {}", x+y+z);
     
     println!("helloworld");
+
+    let mut p = Person{
+        name: String::from("barryz"), 
+        email: String::from("barryzxb@gmail.com"),
+        age: 28,
+        address: String::from("Shanghai China"),
+    };
+
+    println!("==========================");
+
+    println!("person is {:#?}", p);
+
+    // modify p, so p must be mutable.
+    p.add_age();
+
+    println!("after person's adding, the  age is {}", p.age);
+
+    let ap = Person{
+        name: String::from("barryz1"), 
+        email: String::from("barryzxb1@gmail.com"),
+        age: 20,
+        address: String::from("Shanghai China"),
+    };
+
+    if p.older(&ap) {
+        println!("{} is older than {}", p.name, ap.name);
+    } else {
+        println!("{} is younger than {}", p.name, ap.name);
+    }
+
 }
 
 // guess_name function of gusess_name.
